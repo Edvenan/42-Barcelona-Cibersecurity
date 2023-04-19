14804ef1c02ef44219a48ea4831bd2a5990e96fc
evendrel <evendrell@student.42barcelona.com>
2023-04-19 16:26:27 +0200

    Latest the_bank.py update

--- module_01/ex05/the_bank.py
+++ module_01/ex05/the_bank.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 '''
-The goals of this exercise is to discover new built-in functions and deepen your class
-manipulation and to be aware of possibility to modify instanced objects.
+The goals of this exercise is to discover new built-in functions and deepen your
+class manipulation and to be aware of possibility to modify instanced objects.
 In this exercise you learn how to modify or add attributes to an object.
 
 
@@ -28,3 +28,68 @@ class Account(object):
 
     def transfer(self, amount):
         self.value += amount
+        
+
+class Bank(object):
+    """The bank"""
+    def __init__(self):
+        self.accounts = []
+
+    def add(self, new_account):
+        """ Add new_account in the Bank
+            @new_account: Account() new account to append
+            @return True if success, False if an error occured
+        """
+        # test if new_account is an Account() instance and if
+        # it can be appended to the attribute accounts
+        if not isinstance(new_account, Account) or new_account in self.accounts:
+            # The account to be added is either not an Account object
+            # or it already exists
+            return False
+        else:
+            self.accounts.append(new_account)
+    
+    def transfer(self, origin, dest, amount):
+        """" Perform the fund transfer
+            @origin: str(name) of the first account
+            @dest: str(name) of the destination account
+            @amount: float(amount) amount to transfer
+            @return True if success, False if an error occured
+        """
+        # ... Your code ...
+        origin_corrupted = False
+        dest_corrupted = False
+        
+        # corrupted if
+        # account attributes are even 
+        if len(origin.__dict__) % 2 == 0:
+            origin_corrupted = True
+        if len(dest.__dict__) % 2 == 0:
+            dest_corrupted = True
+        
+        #• an attribute starting with b,
+        if sum(char[0].lower() == 'b' for char in origin.__dict__) > 0: 
+            origin_corrupted = True
+        if sum(char[0].lower() == 'b' for char in dest.__dict__) > 0: 
+            dest_corrupted = True
+            
+        #• no attribute starting with zip or addr,
+        if sum(char[0:3].lower() == 'zip' for char in origin.__dict__) == 0 \
+            or sum(char[0:4].lower() == 'addr' for char in origin.__dict__) == 0:
+            origin_corrupted = True
+        if sum(char[0:3].lower() == 'zip' for char in dest.__dict__) == 0 \
+            or sum(char[0:4].lower() == 'addr' for char in dest.__dict__) == 0:
+            dest_corrupted = True
+        
+        #• no attribute name, id and value,
+        #• name not being a string,
+        #• id not being an int,
+        #• value not being an int or a float.
+        #  stores enough money to complete the transfer.
+
+    def fix_account(self, name):
+        """ fix account associated to name if corrupted
+            @name: str(name) of the account
+            @return True if success, False if an error occured
+        """
+        # ... Your code ...
